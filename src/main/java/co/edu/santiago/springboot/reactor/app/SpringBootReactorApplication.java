0b4c6404f5383c6e9b84027b4068b611b34bf93c
santiago-restrepo <sanpra1208@gmail.com>
2020-10-18 18:34:23 -0500

    3. Create our first flux and got subscribed

--- src/main/java/co/edu/santiago/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/co/edu/santiago/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,26 @@
 package co.edu.santiago.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// We declare our String flux.
+		// When a String arrives, then print out the name.
+		Flux<String> names = Flux.just("Jhon", "Diana", "Kate", "Mark")
+				.doOnNext(System.out::println);
+
+		// We got subscribed to our flux. Without subscribers, the onNextMethod won't work.
+		names.subscribe();
+
+	}
 }
