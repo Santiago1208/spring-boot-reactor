f0b26f8b8def7b2db6e6de96fa1743c8c822978c
santiago-restrepo <sanpra1208@gmail.com>
2020-10-18 18:54:03 -0500

    4. Add subscribe actions
    Subscribers can execute some operations when producers produce elements. In the example, we logged out each name when it arrives and define a way to handle errors if they occurs.
    
    **Important:** when producers found errors, the data transmission to subscribers will end.

--- src/main/java/co/edu/santiago/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/co/edu/santiago/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package co.edu.santiago.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,8 @@ import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
+
+	private final static Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
@@ -16,11 +20,15 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// We declare our String flux.
 		// When a String arrives, then print out the name.
-		Flux<String> names = Flux.just("Jhon", "Diana", "Kate", "Mark")
-				.doOnNext(System.out::println);
+		Flux<String> names = Flux.just("Jhon", "", "Diana", "Kate", "Mark", "")
+				.doOnNext(name -> {
+					if (name.isEmpty()) {
+						throw new RuntimeException("Error: Empty name was found");
+					}
+				});
 
 		// We got subscribed to our flux. Without subscribers, the onNextMethod won't work.
-		names.subscribe();
+		names.subscribe(log::info, error -> log.error(error.getMessage()));
 
 	}
 }
